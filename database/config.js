a364ec0161ba8bc35ffe825f7affd42f2191573c
Milegonzalez <mmilano1809@gmail.com>
2019-02-19 10:19:53 -0500

    completed post

--- database/config.js
+++ database/config.js
@@ -12,13 +12,12 @@ mongoose.connect('mongodb://localhost/schedule', option)
     console.log('error - connection ended')
   });
 let db = mongoose.connection;
+mongoose.Promise = global.Promise;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
   // we're connected!
   console.log('Connected!')
 });
-let faker = require('faker');
-
 
 let scheduleSchema = new mongoose.Schema({
   vs: String,
@@ -45,100 +44,5 @@ let scheduleSchema = new mongoose.Schema({
 
 let Schedule = mongoose.model('schedule', scheduleSchema);
 
-let generateTeamFunc = function (number) {
-  let teams = [
-   "Arizona Cardinals",
-   "Atlanta Falcons ",
-   "Baltimore Ravens",
-   "Buffalo Bills",
-   "Carolina Panthers",
-   "Chicago Bears",
-   "Cincinnati Bengals",
-   "Cleveland Browns",
-   "Dallas Cowboys",
-   "Denver Broncos",
-   "Detroit Lions",
-   "Green Bay Packers",
-   "Houston Texans",
-   "Indianapolis Colts",
-   "Jacksonville Jaguars",
-   "Kansas City Chiefs",
-   "Los Angeles Chargers",
-   "Los Angeles Rams",
-   "Miami Dolphins",
-   "Minnesota Vikings",
-   "New England Patriots",
-   "New Orleans Saints",
-   "New York Giants",
-   "New York Jets",
-   "Oakland Raiders",
-   "Philadelphia Eagles",
-   "Pittsburgh Steelers",
-   "San Francisco 49ers",
-   "Seattle Seahawks",
-   "Tampa Bay Buccaneers",
-   "Tennessee Titans",
-   "Washington Redskins"]
-
-   return teams[number]
- }
-
- let generateRandomNumber = function (num) {
-   return Math.floor(Math.random() * num);
- }
-
-const recordGeneratingFunc = function (numberOfRecords) {
-  let results = [];
-  for (let i = 0; i < numberOfRecords; i++) {
-    let record = {
-      vs: " vs ",
-      city: faker.address.city(),
-      team: generateTeamFunc(generateRandomNumber(32)),
-      teamlogo: faker.image.sports(),
-      week: "23:16:44",
-      date: "28-Oct-2018",
-      opponent: generateTeamFunc(generateRandomNumber(32)),
-      opponentlogo: faker.image.sports(),
-      result: generateRandomNumber(10) + "-" + generateRandomNumber(10),
-      winloss: generateRandomNumber(10) + "-" + generateRandomNumber(10),
-      wl: " L",
-      link: faker.internet.url(),
-      feed: faker.lorem.words(),
-      playerpass: faker.name.lastName(),
-      playerrush: faker.name.lastName(),
-      pass: generateRandomNumber(200),
-      rush: generateRandomNumber(200),
-      rec: generateRandomNumber(200),
-    }
-    results.push(record);
-  }
-  return results;
-}
-
-let insert10000Records = function (numberOfTimes) {
-  for (let i = 0; i < numberOfTimes; i++){
-    Schedule.collection.insert(recordGeneratingFunc(10000));
-  }
-  console.log('done!');
-}
-
-// insert10000Records(50)
-
-function resolveAfter2Seconds() {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve(insert10000Records(20));
-    }, 2000);
-  });
-}
-
-async function asyncCall(n) {
-  console.log('starting inserting records ', new Date());
-  for(var i = 0; i < n; i++){
-  var result = await resolveAfter2Seconds();
-  }
-  console.log('10M records inserted ' , new Date());
-  // expected output: 'resolved'
-}
-
-console.log(asyncCall(25));
+module.exports = db;
+module.exports = Schedule;
--- server/index.js
+++ server/index.js
@@ -1,105 +1,8 @@
-// // const express = require('express');
-// // const bodyParser = require('body-parser');
-// // // const ScheduleDB = require('../database/Models/ScheduleDB.js');
-// // const app = express();
-// // const port = process.env.PORT || 3000;
-// // // const knex = require('knex');
-// // const faker = require('faker');
-// // //just added these
-// // // const pg = require('pg');
-// // const cors = require('cors');
-
-
-
-// // app.use(cors());
-// // app.use(express.static(`${__dirname}/../client/dist`));
-// // app.use(bodyParser.json());
-// // app.use(bodyParser.urlencoded({ extended: false }));
-
-
-// // // Schedule endpoint
-// // app.get('/schedule', (req, res) => {
-// //   res.json({info: 'Node.js, Expres and postgres schedule running'})
-// // });
-
-// // app.post('/schedule', (req, res) => {
-// //     knex('schedule').insert({
-// //       vs: " vs ",
-// //       city: "Boston",
-// //       team: "Hello",
-// //       teamlogo: "Hello",
-// //       week: "23:16:44",
-// //       date: "28-Oct-2018",
-// //       opponent: "Hello",
-// //       opponentlogo: "Hello",
-// //       result: 7 +  "-"  + 7,
-// //       winloss: 7 +  "-"  + 6,
-// //       wl: " L",
-// //       link: "Hello",
-// //       feed: "Hello",
-// //       playerpass: "Hello",
-// //       playerrush: "Hello",
-// //       pass: 0,
-// //       rush: 98,
-// //       rec: 90
-// //       // vs: `${faker.name.lastName()}`,
-// //       // city: `${faker.address.city()}`,
-// //       // team: `${faker.name.lastName()}`,
-// //       // teamlogo: `${faker.image.sports()}`,
-// //       // week:`${faker.date.recent()}`,
-// //       // date: `${faker.date.recent()}`,
-// //       // opponent: `${faker.name.lastName()}`,
-// //       // opponentlogo: `${faker.image.sports()}`,
-// //       // result: `${faker.name.lastName()}`,
-// //       // winloss: `${faker.name.lastName()}`,
-// //       // wl: `${faker.name.lastName()}`,
-// //       // link: `${faker.internet.url()}`,
-// //       // feed: `${faker.lorem.words()}`,
-// //       // playerpass: `${faker.name.lastName()}`,
-// //       // playerrush: `${faker.name.lastName()}`,
-// //       // pass: `${faker.random.number()}`,
-// //       // rush: `${faker.random.number()}`,
-// //       // rec: `${faker.random.number()}`,
-// //     })
-// //     .then(() => {
-// //       knex.select().from('schedule').orderBy('timestamp').limit(10)
-// //       .then((data) => {
-// //         res.send(data);
-// //       });
-// //     })
-// // });
-
-
-// // app.listen(port, () => {
-// //   console.log(`listening on port ${port}`);
-// // });
-
-// const express = require('express');
-// const app = express();
-// const port = 3000;
-// const db = require('../database/config');
-
-// // const MongoClient = require('mongodb', { useNewUrlParser: true }).MongoClient
-
-// // MongoClient.connect('mongodb://localhost:27017/schedule', function (err, db) {
-// //   if (err) throw err
-
-// //   db.collection('schedule').find().toArray(function (err, result) {
-// //     if (err) throw err
-
-// //     console.log(result)
-// //   })
-// // })
-
-// app.get('/', (req, res) => res.send('Hello World!'))
-
-// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-
 const express = require('express');
 const bodyParser = require('body-parser');
-const ScheduleDB = require('../database/config');
-
+const Schedule = require('../database/config.js');
+const db = require('../database/config.js');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -109,11 +12,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 
 // Schedule endpoint
-app.get('/espn/schedules', (req, res) => {
-  ScheduleDB.find({}, (err, data) => {
+app.get('/schedule', (req, res) => {
+  Schedule.find({}, (err, data) => {
   })
-    .limit(17)
-    .sort({ week: 1 })
+    .limit(10)
+    // .sort({ week: 1 })
     .then((data) => {
       res.send(data);
     })
@@ -121,6 +24,26 @@ app.get('/espn/schedules', (req, res) => {
       console.err(err);
     });
 });
+
+app.post('/schedule', (req, res) => {
+ const newSchedule = new db(req.body);
+ newSchedule.save(err => {
+   if(err) {
+     res.status(500).send(err);
+   } else {
+     res.status(201).send(newSchedule)
+   }
+ })
+    // .then(() => {
+    //   console.log('record inserted!');
+    // })
+    // .catch((err) => {
+    //   console.err(err);
+    // });
+});
+
+
+
 
 
 const port = process.env.PORT || 3000;
